6bba56b8eb99e5fdfd4c6428b1b9ca8d4be72d45
novajade <zzongboy@naver.com>
2019-06-12 17:32:04 +0900

    Learning Node.js

---
+++ Learning NodeJS/Practice/app.js
@@ -0,0 +1,8 @@
+var stuff = require('./stuff');
+
+console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
+console.log(stuff.adder(3,4));
+console.log(stuff.adder(3, stuff.pi));
+
+//next lesson - > will be #8
+//link: https://www.youtube.com/watch?v=9UaZtgB5tQI&list=PL4cUxeGkcC9gcy9lrvMJ75z9maRw4byYp&index=7
